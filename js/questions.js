31b7ccc515bff02877cc743785f01bbf27a8ff82
kaia000 <kaia000@yahoo.com>
2021-06-26 01:43:57 +0900

    test

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What is your name1?",
+        answer: "hodu",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "hodu"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is your name2?",
+        answer: "hodululu",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "hodululu"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is your name3?",
+        answer: "min",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "min"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What is your name4?",
+        answer: "hoho",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "hoho"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is your name5?",
+        answer: "haha",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "haha"
+        ]
+    }
+]
+
---
+++ js/questions2.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Whaaaaaat is your name1?",
+        answer: "ninjin",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "hodu"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Whaaaaaaaat is your name2?",
+        answer: "ninjin",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "hodululu"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Whaaaaaaat is your name3?",
+        answer: "ninjin",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "min"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Whaaaaaaaaat is your name4?",
+        answer: "ninjin",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "hoho"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Whaaaaaaaaaat is your name5?",
+        answer: "ninjin",
+        options: [
+                "ninjin",
+                "yeojeee",
+                "yeojin",
+                "haha"
+        ]
+    }
+]
+
